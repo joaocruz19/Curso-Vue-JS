8c1126d208baece89ec1a7030ccd9aad4cc81308
joaocruz19 <joaovictorcruz82@gmail.com>
2020-04-10 13:40:38 -0300

    Started chapter 7

---
+++ componentes-comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuário: <strong>{{inverterNome()}}</strong></p>
+        <p>Idade do Usuário: <strong>{{idade}}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn">Reiniciar Nome (Callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            default: "Anônimo",
+            // required: true,
+            // default: function() {
+            //     return Array(10).fill(0).join(",");
+            // }
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split("").reverse().join("");
+        },
+        reiniciarNome() {
+            this.nome = "Jodie";
+            this.$emit("nomeMudou", this.nome);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
---
+++ componentes/README.md
@@ -0,0 +1,8 @@
+# Links da Documentação
+Links dos tópicos abordados no capítulo para a documentação oficial do Vue.
+
+### Componentes
+[Documentação Oficial](https://br.vuejs.org/v2/guide/components.html)
+
+### Registro de Componentes
+[Documentação Oficial](https://br.vuejs.org/v2/guide/components-registration.html)
